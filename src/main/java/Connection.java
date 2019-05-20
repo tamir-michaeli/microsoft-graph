a4aaca9b39d44e5d03f1e37f26d122896e058aff
Hanan Egbaria <hanan.egbaria@logz.io>
2019-05-20 12:09:30 +0300

    connection test

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -1,8 +1,10 @@
-import org.junit.Assert;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 
@@ -10,7 +12,8 @@ public class Connection {
     private final String tenantId;
     private final String clientId;
     private final String clientSecret;
-    //private final String resource; //FIXME should check if this mandatory
+
+    private static HttpURLConnection con;
 
 
     public Connection(String tenantId, String clientId, String clientSecret) {
@@ -24,33 +27,52 @@ public class Connection {
         String path = "https://login.microsoftonline.com/" + tenantId + "/oauth2/token";
         String body = "grant_type=client_credentials&client_id=" + clientId + "&client_secret=" + clientSecret;
         byte[] requestBody = body.getBytes("UTF-8");
+        String responseMessage;
 
-        //post method
-        URL url = null;
         try {
-            int responseCode;
-            String responseMessage;
-            url = new URL(path);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
+            URL myurl = new URL(path);
+            con = (HttpURLConnection) myurl.openConnection();
 
-            conn.getOutputStream().write(requestBody);
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", "Java client");
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-            responseCode = conn.getResponseCode();
-            responseMessage = conn.getResponseMessage();
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(requestBody);
+            }
 
-            return responseMessage;
-        } catch (MalformedURLException e) {
-            Assert.fail(e.getMessage());
-        } catch (IOException e) {
-            Assert.fail(e.getMessage());
-        } catch (Exception e) {
-            throw new Exception(e);
+            StringBuilder content;
+
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()))) {
+
+                String line;
+                content = new StringBuilder();
+
+                while ((line = in.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+            responseMessage = content.toString();
+
+
+        } finally {
+
+            con.disconnect();
         }
-        return null;
+        return extractToken(responseMessage);
+    }
+
+    private String extractToken(String responseMessage) {
+        JsonObject jsonObject;
+        try {
+            jsonObject = new JsonParser().parse(responseMessage).getAsJsonObject();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Not a valid json received in body of request", e);
+        }
+        return jsonObject.get("access_token").toString();
     }
 }
--- src/test/java/ConnectionTest.java
+++ src/test/java/ConnectionTest.java
@@ -5,8 +5,8 @@ public class ConnectionTest {
 
     @Test
     public void connectionTest() throws Exception {
-        Connection connection = new Connection( "c96a62e5-1e49-4187-b394-08b694e8bb0d", "323a95a2-22eb-46c7-a80b-1ebeadc88ca8", "dsvSLXZdaKJAJ5GrSS29bd1h7k89u4sDMOiglihZkzE=");
-        String response = connection.connect();
-        Assert.assertNotNull(response);
+        Connection connection = new Connection("c96a62e5-1e49-4187-b394-08b694e8bb0d", "323a95a2-22eb-46c7-a80b-1ebeadc88ca8", "dsvSLXZdaKJAJ5GrSS29bd1h7k89u4sDMOiglihZkzE=");
+        String token = connection.connect();
+        Assert.assertNotNull(token);
     }
 }
