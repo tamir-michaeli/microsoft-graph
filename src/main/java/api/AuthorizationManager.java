9e132ccb2e4215640eb561fd92892bd95373ef9f
yyyogev <yogev.metzuyanim@logz.io>
2019-07-11 16:50:55 +0300

    new access token methodology

--- src/main/java/api/AuthorizationManager.java
+++ src/main/java/api/AuthorizationManager.java
@@ -1,67 +1,102 @@
 package api;
 
-import com.microsoft.aad.adal4j.AuthenticationContext;
-import com.microsoft.aad.adal4j.AuthenticationResult;
-import com.microsoft.aad.adal4j.ClientCredential;
+import org.apache.http.HttpException;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class AuthorizationManager {
 
-    private static final String MICROSOFTONLINE_ADDRESS = "https://login.microsoftonline.com/";
-    private static final String RESOURCE = "https://manage.office.com";
     private static final Logger logger = LoggerFactory.getLogger(AuthorizationManager.class.getName());
 
+    private static final String MICROSOFTONLINE_ADDRESS = "https://login.microsoftonline.com/";
+    private static final String SCOPE = "https://graph.microsoft.com/.default";
+    private static final String OAUTH2_TOKEN_API = "/oauth2/v2.0/token";
+    public static final String CLIENT_CREDENTIALS_GRANT_TYPE = "client_credentials";
+    public static final String REQUEST_CONTENT_TYPE = "Content-Type";
+    public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
+    public static final String REQUEST_CONTENT_LENGTH = "Content-Length";
+    public static final String JSON_ACCESS_TOKEN = "access_token";
+    public static final String JSON_ACCESS_TOKEN_EXPIRE_DURATION = "expires_in";
 
+    private final String clientId;
+    private final String clientSecret;
+    private final String tenantId;
     private String accessToken;
-    private AuthenticationContext context;
     private long currentTokenExpiry = 0;
-    private Future<AuthenticationResult> future;
 
 
     public AuthorizationManager(String tenantId, String clientId, String clientSecret) {
-        String authority = MICROSOFTONLINE_ADDRESS + tenantId;
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        logger.debug("creating authorization context");
-        try {
-            context = new AuthenticationContext(authority, true, service);
-        } catch (MalformedURLException e) {
-            logger.error("malformed authority url error: {}", e.getMessage(), e);
-        }
-        future = context.acquireToken(AuthorizationManager.RESOURCE,
-                new ClientCredential(clientId, clientSecret), null);
-        try {
-            accessToken = future.get().getAccessToken();
-            System.out.println("got access token exp:" + future.get().getExpiresAfter() + " token: " + accessToken);
-            currentTokenExpiry = System.currentTimeMillis() + future.get().getExpiresAfter()*1000;
-        } catch (ExecutionException | InterruptedException e) {
-            logger.error("Error fetching access token: ", e.getMessage(), e);
-        }
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.tenantId = tenantId;
+
+        retrieveToken();
     }
 
     public String getAccessToken() {
         if (System.currentTimeMillis() > currentTokenExpiry - 5 * 60 * 1000) { // 5 minutes safety
-            if (!refreshToken()) {
+            if (!retrieveToken()) {
                 return null;
             }
         }
         return accessToken;
     }
 
-    private boolean refreshToken() {
+    private boolean retrieveToken() {
+
+        String url = MICROSOFTONLINE_ADDRESS + tenantId + OAUTH2_TOKEN_API;
+
         try {
-            accessToken = future.get().getRefreshToken();
-            System.out.println("got refresh token: " + accessToken);
-            currentTokenExpiry = System.currentTimeMillis() + future.get().getExpiresAfter()*1000;
-            return true;
-        } catch (InterruptedException | ExecutionException e) {
-            logger.error("Error refreshing access token: ", e.getMessage(), e);
+            String urlParameters  = "grant_type=" + CLIENT_CREDENTIALS_GRANT_TYPE
+                    + "&client_id=" + clientId
+                    + "&scope=" + SCOPE
+                    + "&client_secret=" + clientSecret;
+            byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+            int postDataLength = postData.length;
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setDoOutput(true);
+            con.setRequestProperty(REQUEST_CONTENT_TYPE, APPLICATION_FORM_URLENCODED);
+            con.setRequestProperty(REQUEST_CONTENT_LENGTH, Integer.toString(postDataLength));
+
+            try( DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write( postData );
+            }
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                JSONObject jsonResponse = new JSONObject(response.toString());
+                accessToken = jsonResponse.get(JSON_ACCESS_TOKEN).toString();
+                System.out.println(accessToken); //TODO REMOVE THIS
+                currentTokenExpiry = System.currentTimeMillis() + jsonResponse.getInt(JSON_ACCESS_TOKEN_EXPIRE_DURATION)*1000;
+                return true;
+            } else {
+                throw new HttpException("Invalid response code: " + con.getResponseCode() + "\n response: " + response);
+            }
+
+        } catch (IOException | JSONException | HttpException e) {
+            logger.error("Error fetching access token: {}", e.getMessage(), e);
         }
         return false;
     }
