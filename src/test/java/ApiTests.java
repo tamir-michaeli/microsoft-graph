62ee6dce258a35313be8724b62a59e45bd00586a
yyyogev <yogev.metzuyanim@logz.io>
2019-07-22 11:20:47 +0300

    access token request test

---
+++ src/test/java/ApiTests.java
@@ -0,0 +1,46 @@
+import api.AuthorizationManager;
+import objects.AzureADClient;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.naming.AuthenticationException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class ApiTests {
+
+    private static MockWebServer mockWebServer = new MockWebServer();
+
+    @BeforeClass
+    public static void startMockServer() {
+        try {
+            mockWebServer.start(8123);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void accessTokenRequestTest() throws InterruptedException, UnsupportedEncodingException {
+        String cliendID = "1234-5678";
+        String clientSecret = "shh don't tell";
+        AzureADClient client = new AzureADClient();
+        client.setClientId(cliendID);
+        client.setClientSecret(clientSecret);
+        client.setTenantId("aaa-bbb");
+        mockWebServer.enqueue(new MockResponse());
+        Assert.assertThrows(AuthenticationException.class, () -> {
+            AuthorizationManager manager = new AuthorizationManager(client,"http://localhost:8123");
+        });
+        RecordedRequest request = mockWebServer.takeRequest();
+        String body = request.getBody().readUtf8();
+        Assert.assertTrue(body.contains("client_id=" + cliendID));
+        Assert.assertTrue(body.contains("client_secret=" + URLEncoder.encode(clientSecret, StandardCharsets.UTF_8.toString())));
+    }
+}
